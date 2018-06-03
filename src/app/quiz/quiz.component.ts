0ddc8fb19c45e3b12200325c0c11dbb6b05fb387
DESKTOP-8L8E8H5\spiki <gewoongram@gmail.com>
2018-06-03 15:38:16 +0200

    back button werkt

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -26,81 +26,71 @@ export class QuizComponent implements OnInit {
       let score = 0;
 
       // if the restart button is pressed, start quiz again
+      $('.back').click(function() {
+
+        console.log('backbutton');
+
+        const previousCard = parseInt($('.progress-bar').attr('card'), 10 ) - 1;
+        const currentCard = previousCard + 1;
+
+        // remove last answer from answers array
+        answers.pop();
+
+        progress(previousCard, answers); // update progress bar
+
+        // remove the back button
+        if (previousCard === 0) {
+          $('.back').addClass('hidden');
+        }
+
+        $('.card-' + (currentCard + 1)).fadeOut();
+        $('.card-' + currentCard).delay(450).removeClass('hidden').hide().fadeIn();
+      });
 
 
 
 
       // forward
-      $('.btn').click(function() {
-        if ($('.back') === true){
+      $('.btn-block').click(function() {
+        console.log('iets');
+        let currentCard = $(this).closest('[class^="card-"]').attr('class');
+        console.log(currentCard);
+        currentCard = parseInt(currentCard.split('-')[1], 10);
+        console.log(currentCard);
 
-          console.log('backbutton');
+        // add answer to answers array
+        const answer = $(this).text();
+        answers[currentCard] = answer;
 
-          const previousCard = parseInt($('.progress-bar').attr('card'), 10 ) - 1;
-          const currentCard = previousCard + 1;
+        progress(currentCard, answers); // update progress bar
 
-          // remove last answer from answers array
-          answers.pop();
-
-          progress(previousCard, answers); // update progress bar
-
-          // remove the back button
-          if (previousCard === 0) {
-            $('.back').addClass('hidden');
-          }
-
-          $('.card-' + (currentCard + 1)).fadeOut();
-          $('.card-' + currentCard).delay(450).removeClass('hidden').hide().fadeIn();
-        } else {
-          console.log('iets');
-          let currentCard = $(this).closest('[class^="card-"]').attr('class');
-          console.log(currentCard);
-          currentCard = parseInt(currentCard.split('-')[1], 10);
-          console.log(currentCard);
-
-          // add answer to answers array
-          const answer = $(this).text();
-          answers[currentCard] = answer;
-
-          progress(currentCard, answers); // update progress bar
-
-          // add in back button
-          if (currentCard !== 0 && $('.back').hasClass('hidden')) {
-            $('.back').removeClass('hidden');
-          }
-
-
-          $('.card-' + currentCard).fadeOut();
-
-          if (currentCard === 13) {
-            $('.theEnd').delay(450).removeClass('hidden').hide().fadeIn();
-
-            for (let i = 0; i < answers.length; i++) {
-              const temp = answers[i];
-              if (temp == 'Ja') {
-                score = score + 1;
-              }
-              // Do something
-            }
-            if (score <= 6) {
-              $('#score').html('Je hebt weinig symptomen');
-            }
-            if (score >= 6 && score <= 12) {
-              $('#score').html('Je hebt veel symptomen. Een afspraak bij de huisarts is een goed idee');
-            }
-          }
-          $('.card-' + (currentCard + 1)).delay(450).removeClass('hidden').hide().fadeIn();
+        // add in back button
+        if (currentCard !== 0 && $('.back').hasClass('hidden')) {
+          $('.back').removeClass('hidden');
         }
 
 
+        $('.card-' + currentCard).fadeOut();
+
+        if (currentCard === 13) {
+          $('.theEnd').delay(450).removeClass('hidden').hide().fadeIn();
+
+          for (let i = 0; i < answers.length; i++) {
+            const temp = answers[i];
+            if (temp == 'Ja') {
+              score = score + 1;
+            }
+            // Do something
+          }
+          if (score <= 6) {
+            $('#score').html('Je hebt weinig symptomen');
+          }
+          if (score >= 6 && score <= 12) {
+            $('#score').html('Je hebt veel symptomen. Een afspraak bij de huisarts is een goed idee');
+          }
+        }
+        $('.card-' + (currentCard + 1)).delay(450).removeClass('hidden').hide().fadeIn();
       });
-
-      // backwards
-      $('.back').click(function() {
-
-
-      });
-
     });
 
     function progress(card, answers) {
