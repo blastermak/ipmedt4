495e9aa1e14d44790fc532bac3044bb2afae1544
gewoongram <gewoongram@gmail.com>
2018-06-07 13:11:05 +0200

    dagboek test pagina

--- src/app/dagboek/dagboek.component.ts
+++ src/app/dagboek/dagboek.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-dagboek',
@@ -8,6 +10,13 @@ import { Component, OnInit } from '@angular/core';
 export class DagboekComponent implements OnInit {
 
   constructor() { }
+
+  model: NgbDateStruct;
+  date: {year: number, month: number};
+
+  selectToday() {
+    this.model = {year: now.getFullYear(), month: now.getMonth() + 1, day: now.getDate()};
+  }
 
   ngOnInit() {
   }
