ba770681c6d17ca7c07e0de678b394439e6e4b74
DESKTOP-8L8E8H5\spiki <gewoongram@gmail.com>
2018-06-14 23:40:53 +0200

    error

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
 import {Chart} from 'chart.js';
-import { Component } from '/.dagboek.component';
 
-declare var require: any
+
+declare var require: any;
 
 //npm install chart.js --save
 
